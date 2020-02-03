460935dc3c9747a24538776c78771b35632bfb9d
SaiProton <40346418+SaiProton@users.noreply.github.com>
2020-02-02 22:08:52 -0500

    3rd Person Camera!!
    Its somehow not completely broken now

---
+++ The Vacuum Of Space/Assets/Scripts/Environment/CameraCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    public float minDist = 1.0f;
+    public float maxDist = 4.0f;
+    public float smooth = 10.0f;
+
+    private Vector3 dollyDir;
+    private float distance;
+
+    void Awake()
+    {
+        dollyDir = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+    }
+
+    void Update()
+    {
+        Vector3 desiredPos = transform.parent.TransformPoint(dollyDir * maxDist);
+        RaycastHit hit;
+
+        if (Physics.Linecast(transform.parent.position, desiredPos, out hit))
+        {
+            distance = Mathf.Clamp((hit.distance * 0.9f), minDist, maxDist);
+        } 
+        else
+        {
+            distance = maxDist;
+        }
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * distance, Time.deltaTime * smooth);
+    }
+}
---
+++ The Vacuum Of Space/Assets/Scripts/Environment/CameraFollow.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject camFollow;
+    public float camSpeed = 120.0f;
+    public float clampAngle = 80.0f;
+    public float mouseSensitivity = 150.0f;
+
+    private float mouseX;
+    private float mouseY;
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
+
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+
+        rotY = rot.y;
+        rotX = rot.x;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    void LateUpdate()
+    {
+        CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        Transform target = camFollow.transform;
+
+        float step = camSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
