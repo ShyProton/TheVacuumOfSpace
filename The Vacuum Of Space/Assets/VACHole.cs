c4cb7266eab98c32fd101a74d14c4c4f812e1931
SaiProton <40346418+SaiProton@users.noreply.github.com>
2020-02-21 03:29:29 -0500

    VACM animations
    for activating and deactivating graviton  surge

---
+++ The Vacuum Of Space/Assets/VACHole.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VACHole : MonoBehaviour
+{
+    public Animator vacimation;
+
+    private bool mouseClicked;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        mouseClicked = Input.GetMouseButtonDown(0);
+
+        if(Input.GetMouseButton(0))
+        {
+            vacimation.SetTrigger("MouseClicked");
+        }
+
+        if(!Input.GetMouseButton(0))
+        {
+            vacimation.ResetTrigger("MouseClicked");
+        }
+    }
+}
