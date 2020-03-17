f13e2b2259b7425334ff2189cf6d061baa53170e
SaiProton <40346418+SaiProton@users.noreply.github.com>
2020-03-16 22:09:33 -0400

    music added
    space piano time

---
+++ The Vacuum Of Space/Assets/Scripts/Environment/Global/MusicMain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicMain : MonoBehaviour
+{
+
+    public AudioSource defaultSong;
+
+    void Start()
+    {
+        defaultSong.Play();
+    }
+
+    void Update()
+    {
+        
+    }
+}
